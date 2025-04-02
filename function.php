b8c6a9cc0356570ee002938973a864d15f46ad4e
Cristian Trolese <borderlands96@outlook.it>
2025-04-02 13:52:31 +0200

    Aggiunta funzione divisione ma con controllo

--- function.php
+++ function.php
@@ -15,4 +15,12 @@ function sottrazzione(n1,n2){
 function moltiplicazione(n1,n2){
     return n1*n2;
 }
+
+function divisione(n1,n2){
+    if(n2 !== 0)
+    return n1/n2;
+}
+    else{
+        return="error";
+    }
 ?>
